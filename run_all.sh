aa536896a8249d9b96f60583582b399a6dc0045e
Karl Lundstig <karl@lundstig.com>
2020-08-04 13:32:58 +0200

    run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,10 @@
+echo Example
+time ./solver.o < inputs/example.in > out/example.out
+echo Working
+time ./solver.o < inputs/me_working_from_home.in > out/me_working_from_home.out
+echo Cooking
+time ./solver.o < inputs/learning_cooking_from_youtube.in > out/learning_cooking_from_youtube.out
+echo Vloggers
+time ./solver.o < inputs/vloggers_of_the_world.in > out/vloggers_of_the_world.out
+echo Music
+time ./solver.o < inputs/music_videos_of_2020.in > out/music_videos_of_2020.out
